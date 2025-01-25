3ef26be39a3b70f080baf7918f62d0750c3fda2c
MrJokester <mrjokester01010@gmail.com>
2025-01-25 07:19:23 +0200

    Create index.js

---
+++ apis/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const catRouter = require("./apis/cat");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use("/cats", express.static(path.join(__dirname, "public/cats")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/cat", catRouter);
+
+app.listen(PORT, () => {
+  console.log(`Cat API running at http://localhost:${PORT}`);
+});
