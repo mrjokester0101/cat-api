bb19adc7aaf802f2fe9328b8ee66af766b0714c3
MrJokester <mrjokester01010@gmail.com>
2025-01-25 07:37:02 +0200

    Update and rename cat.js to random.js

---
+++ apis/random.js
@@ -0,0 +1,14 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+  const catsDir = path.join(process.cwd(), "public/cats");
+
+  fs.readdir(catsDir, (err, files) => {
+    if (err) return res.status(500).json({ error: "Failed to read images" });
+    if (!files.length) return res.status(404).json({ error: "No images found" });
+
+    const randomFile = files[Math.floor(Math.random() * files.length)];
+    res.json({ image: `/cats/${randomFile}` });
+  });
+}
