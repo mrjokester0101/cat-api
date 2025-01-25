614fe29e2eb26cf5878db2244e075a1213816265
MrJokester <mrjokester01010@gmail.com>
2025-01-25 10:44:49 +0200

    Update cat.js

--- pages/api/cat.js
+++ pages/api/cat.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
   const catsDir = path.join(process.cwd(), 'public', 'cats');
   const catFiles = fs.readdirSync(catsDir);
   const randomCat = catFiles[Math.floor(Math.random() * catFiles.length)];
-  const catImageUrl = `/cats/${randomCat}`;
+  const catImageUrl = `https://cat-api-jet.vercel.app/cats/${randomCat}`;
 
   res.status(200).json({ image: catImageUrl });
 }
