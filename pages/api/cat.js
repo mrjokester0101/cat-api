d50b0c9aa69fd45b01d1749d5c395b9573bcc0dc
MrJokester <mrjokester01010@gmail.com>
2025-01-25 08:18:48 +0200

    Update cat.js

--- pages/api/cat.js
+++ pages/api/cat.js
@@ -9,6 +9,7 @@ export default function handler(req, res) {
     if (!files.length) return res.status(404).json({ error: "No images found" });
 
     const randomFile = files[Math.floor(Math.random() * files.length)];
+
     res.status(200).json({ image: `/cats/${randomFile}` });
   });
 }
