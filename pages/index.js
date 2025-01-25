cff7dd380e1259d15bdf6ac9243aa004fbc1e4d3
MrJokester <mrjokester01010@gmail.com>
2025-01-25 20:47:46 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,19 +16,36 @@ export default function Home() {
   }, []);
 
   return (
-    <div className={styles.home}>
+    <div className={styles.all}>
       <div className={styles.container}>
-        <h1 className={styles.title}>Random Cat Image</h1>
-        {catImage ? (
-          <Image
-            className={styles.catImage}
-            src={catImage}
-            alt="Random Cat"
+        <Head>
+          <title>Random Cats API</title>
+          <meta name="description" content="The Random Cat Images API." />
+          <link rel="icon" href="/cats/cat1.png" />
+        </Head>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Random Cat Image</h1>
+          {catImage ? (
+            <Image
+              className={styles.catImage}
+              src={catImage}
+              alt="Random Cat"
               layout="intrinsic"
-          />
-        ) : (
-          <div className={styles.loading}>Loading...</div>
-        )}
+            />
+          ) : (
+            <div className={styles.loading}>Loading...</div>
+          )}
+            
+            <div className={styles.grid}>
+              <div className={styles.card}>
+                <h2>The API</h2>
+                <a href="https://discord.com/oauth2/authorize?client_id=1251525661185806336&permissions=8&integration_type=0&scope=bot" className={styles.linkcard}>
+                  <p>Get Invite</p>
+                </a>
+              </div>
+           </div>
+          
+        </main>
       </div>
     </div>
   );
