85d6d0e0019a720f85bbb75bafedcc368f03c932
MrJokester <mrjokester01010@gmail.com>
2025-01-25 09:27:45 +0200

    Update cat.js

--- pages/api/cat.js
+++ pages/api/cat.js
@@ -6,5 +6,6 @@ export default function handler(req, res) {
   const catFiles = fs.readdirSync(catsDir);
   const randomCat = catFiles[Math.floor(Math.random() * catFiles.length)];
   const catImageUrl = `/cats/${randomCat}`;
+
   res.status(200).json({ image: catImageUrl });
 }
