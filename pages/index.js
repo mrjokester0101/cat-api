4b830552108870a318a1cf384e8c3cccabdc2783
MrJokester <mrjokester01010@gmail.com>
2025-01-30 22:51:00 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,37 +17,35 @@ const Home = () => {
   }, []);
 
   return (
-    <div className={styles.all}>
-      <div className={styles.container}>
-        <Head>
-          <title>Random Cats API</title>
-          <meta name="description" content="The Random Cat Images API." />
-          <link rel="icon" href="/cats/cat1.png" />
-        </Head>
-        <main className={styles.main}>
-          <h1 className={styles.title}>Random Cat Image</h1>
-          {catImage ? (
-            <Image
-              className={styles.catImage}
-              src={catImage}
-              alt="Random Cat"
-              layout="intrinsic"
-            />
-          ) : (
-            <div className={styles.loading}>Loading...</div>
-          )}
-            
-            <div className={styles.grid}>
-              <div className={styles.card}>
-                <h2>The Random Cat Images API</h2>
-                <a href="https://cat-api-jet.vercel.app/api/cat" className={styles.linkcard}>
-                  <p>Click Me</p>
-                </a>
-              </div>
-           </div>
+    <div className={styles.container}>
+      <Head>
+        <title>Random Cats API</title>
+        <meta name="description" content="The Random Cat Images API." />
+        <link rel="icon" href="/cats/cat1.png" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Random Cat Image</h1>
+        {catImage ? (
+          <Image
+            className={styles.catImage}
+            src={catImage}
+            alt="Random Cat"
+            layout="intrinsic"
+          />
+        ) : (
+          <div className={styles.loading}>Loading...</div>
+        )}
           
-        </main>
-      </div>
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <h2>The Random Cat Images API</h2>
+            <a href="https://cat-api-jet.vercel.app/api/cat" className={styles.linkcard}>
+              <p>Click Me</p>
+            </a>
+          </div>
+        </div>
+        
+      </main>
     </div>
   );
 }
