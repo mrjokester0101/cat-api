5dbae576147460d525037adb723f27a3e9ad0bb2
MrJokester <mrjokester01010@gmail.com>
2025-06-23 10:47:21 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Head from "next/head";
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
